a75128b187c91f94ff02e8307fcc6f06a9de57d4
sanjeet kumar <114237470+sanjeetkumar88@users.noreply.github.com>
2023-11-19 20:16:11 +0530

    Add files via upload

---
+++ AtmInterface.java
@@ -0,0 +1,91 @@
+
+
+import java.util.Scanner;
+
+class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        balance = initialBalance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: $" + amount);
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+    public boolean withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: $" + amount);
+            return true;
+        } else {
+            System.out.println("Invalid withdrawal amount or insufficient balance.");
+            return false;
+        }
+    }
+}
+
+public class AtmInterface{
+    private BankAccount account;
+
+    public AtmInterface(BankAccount account) {
+        this.account = account;
+    }
+
+    public void displayMenu() {
+        System.out.println("Welcome to the ATM!");
+        System.out.println("1. Check Balance");
+        System.out.println("2. Deposit");
+        System.out.println("3. Withdraw");
+        System.out.println("4. Exit");
+    }
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            displayMenu();
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Balance: $" + account.getBalance());
+                    break;
+                case 2:
+                    System.out.print("Enter the deposit amount: $");
+                    double depositAmount = scanner.nextDouble();
+                    account.deposit(depositAmount);
+                    break;
+                case 3:
+                    System.out.print("Enter the withdrawal amount: $");
+                    double withdrawAmount = scanner.nextDouble();
+                    if (account.withdraw(withdrawAmount)) {
+                        System.out.println("Please take your cash.");
+                    }
+                    break;
+                case 4:
+                    System.out.println("Thank you for using the ATM. Goodbye!");
+                    return;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+                    break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        BankAccount userAccount = new BankAccount(1000.0); // Initialize with an initial balance of $1000
+        AtmInterface atm = new AtmInterface(userAccount);
+        atm.run();
+    }
+}
---
+++ numbergame.java
@@ -0,0 +1,80 @@
+
+import java.util.Scanner;
+public class numbergame {
+	// Function that implements the
+	// number guessing game
+	public static void
+	guessingNumberGame()
+	{
+	    boolean playAgain = true;
+        while (playAgain) {
+		// Scanner Class
+		Scanner sc = new Scanner(System.in);
+
+		// Generate the numbers
+		int number = 1 + (int)(100* Math.random());
+
+		// Given K maxattempts
+		int K = 10;
+
+		int i, guess;
+
+		System.out.println(
+			"A number is chosen"
+			+ " between 1 to 100."
+			+ "Guess the number");
+
+		// Iterate over K Trials
+		for (i = 0; i < K; i++) {
+
+			System.out.println(
+				"Enter your guess: ");
+
+
+			// Take User input for guessing
+			guess = sc.nextInt();
+
+			// If the number is guessed
+			if (number == guess) {
+				System.out.println(
+					"Congratulations!"
+					+ " You guessed the number in "+i+" Attempts");
+				break;
+			}
+			else if (number > guess
+					&& i != K - 1) {
+				System.out.println(
+					"The number is "
+					+ "greater than " + guess);
+			}
+			else if (number < guess
+					&& i != K - 1) {
+				System.out.println(
+					"The number is"
+					+ " less than " + guess);
+			}
+		}
+
+		if (i == K) {
+			System.out.println(
+				"You have exhausted"
+				+ " K trials.");
+
+			System.out.println(
+				"The number was " + number);
+		}
+		System.out.print("Do you want to play again? (yes/no): ");
+            String playAgainResponse = sc.next().toLowerCase();
+            playAgain = playAgainResponse.equals("yes");
+        }
+	}
+
+	// Driver Code
+	public static void
+	main(String arg[])
+	{
+
+		// Function Call
+		guessingNumberGame();
+	}
+}
